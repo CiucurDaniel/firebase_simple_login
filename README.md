b4a37ff6d0b3249c653f2db402bfbd492c1fe1f3
Ciucur Daniel <ciucur.daniel14@gmail.com>
2022-01-26 22:37:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# firebase_simple_login
+
+An example of Firebase for authentication for a Flutter project, includes Android, iOS and Web.
---
+++ android/app/src/main/kotlin/com/ciucurdaniel/firebase_simple_login/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ciucurdaniel.firebase_simple_login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
